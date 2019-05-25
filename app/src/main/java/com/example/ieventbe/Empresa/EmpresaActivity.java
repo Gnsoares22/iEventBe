f377610bb5cf1a3149b03e490795c042146a6202
Gabriel Nascimento Soares <gnsoares8@gmail.com>
2019-05-25 18:02:11 -0300

    Estilizando a tela do usuário

--- app/src/main/java/com/example/ieventbe/Empresa/EmpresaActivity.java
+++ app/src/main/java/com/example/ieventbe/Empresa/EmpresaActivity.java
@@ -11,6 +11,8 @@ public class EmpresaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
+
+        getSupportActionBar().hide();
     }
 
 
