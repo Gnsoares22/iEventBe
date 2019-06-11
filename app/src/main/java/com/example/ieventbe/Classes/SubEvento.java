e1cab7b7df382f825a65e97c6efb5f527d6bad78
Gabriel Nascimento Soares <gnsoares8@gmail.com>
2019-06-11 02:46:11 -0300

    Cadastrando Sub evento

--- app/src/main/java/com/example/ieventbe/Classes/SubEvento.java
+++ app/src/main/java/com/example/ieventbe/Classes/SubEvento.java
@@ -1,4 +1,117 @@
 package com.example.ieventbe.Classes;
 
 public class SubEvento {
+
+       private String sub_responsavel;
+       private String sub_local_atividades;
+       private String sub_minicurriculo;
+       private String sub_descricacao;
+       private String sub_periodo;
+       private String sub_foto;
+       private String sub_tipo;
+
+    public SubEvento() {
+
+        //Método construtor
+    }
+
+    //metodo pega todos itens da classe e salva no firebase
+
+    public SubEvento(String res, String lca, String minic, String desc, String per, String foto,
+                     String tipo){
+
+        if(res.trim().equals("")){
+
+            res = "Sub evento sem responsável";
+
+        }
+
+        if(lca.trim().equals("")){
+
+            lca = "Evento sem local";
+
+        }
+
+        if(minic.trim().equals("")){
+
+            minic = "Sem minicorriculo";
+
+        }
+
+        if(desc.trim().equals("")){
+
+            desc = "Sub evento sem descricao";
+
+        }
+
+        if(per.trim().equals("")){
+
+            per = "Evento sem periodo";
+        }
+
+            sub_responsavel = res;
+            sub_local_atividades = lca;
+            sub_minicurriculo = minic;
+            sub_descricacao = desc;
+            sub_periodo = per;
+            sub_foto = foto;
+            sub_tipo = tipo;
+
+    }
+
+    public String getSub_responsavel() {
+        return sub_responsavel;
+    }
+
+    public void setSub_responsavel(String sub_responsavel) {
+        this.sub_responsavel = sub_responsavel;
+    }
+
+    public String getSub_local_atividades() {
+        return sub_local_atividades;
+    }
+
+    public void setSub_local_atividades(String sub_local_atividades) {
+        this.sub_local_atividades = sub_local_atividades;
+    }
+
+    public String getSub_minicorriculo() {
+        return sub_minicurriculo;
+    }
+
+    public void setSub_minicorriculo(String sub_minicorriculo) {
+        this.sub_minicurriculo = sub_minicorriculo;
+    }
+
+    public String getSub_descricacao() {
+        return sub_descricacao;
+    }
+
+    public void setSub_descricacao(String sub_descricacao) {
+        this.sub_descricacao = sub_descricacao;
+    }
+
+    public String getSub_periodo() {
+        return sub_periodo;
+    }
+
+    public void setSub_periodo(String sub_periodo) {
+        this.sub_periodo = sub_periodo;
+    }
+
+    public String getSub_foto() {
+        return sub_foto;
+    }
+
+    public void setSub_foto(String sub_foto) {
+        this.sub_foto = sub_foto;
+    }
+
+    public String getSub_tipo() {
+        return sub_tipo;
+    }
+
+    public void setSub_tipo(String sub_tipo) {
+        this.sub_tipo = sub_tipo;
+    }
 }
--- app/src/main/java/com/example/ieventbe/Empresa/CadastroSubEventoActivity.java
+++ app/src/main/java/com/example/ieventbe/Empresa/CadastroSubEventoActivity.java
@@ -1,21 +1,42 @@
 package com.example.ieventbe.Empresa;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.ieventbe.Classes.Evento;
+import com.example.ieventbe.Classes.SubEvento;
 import com.example.ieventbe.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class CadastroSubEventoActivity extends AppCompatActivity {
 
@@ -27,10 +48,15 @@ public class CadastroSubEventoActivity extends AppCompatActivity {
     private EditText periodosubevento;
     private EditText descricaosubevento;
     private Spinner tiposubevento;
-    private Spinner eventoid;
     Bitmap imagem;
 
     private Uri mImageUri;
+
+    private StorageReference stref;
+    private DatabaseReference ref;
+
+    private StorageTask SubeventosUploadTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +81,11 @@ public class CadastroSubEventoActivity extends AppCompatActivity {
         progresso = findViewById(R.id.Progresso);
         Button btnCadastraSub = (Button) findViewById(R.id.btnCadastraSubEvento);
 
+        //Instanciando a referencia para a classe sub evento
+
+        stref = FirebaseStorage.getInstance().getReference("subevento");
+        ref = FirebaseDatabase.getInstance().getReference("subeventos");
+
 
         //criando lista para tipos de sub eventos para exbir no spinner tipo de sub eventos
 
@@ -63,12 +94,45 @@ public class CadastroSubEventoActivity extends AppCompatActivity {
         tiposubevento.setAdapter(tipos);
 
 
-        //CONTINUAR
+        btnCadastraSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                //verifica se a foto é nula ou se elá está em carregamento ainda
+
+                if (SubeventosUploadTask != null && SubeventosUploadTask.isInProgress()) {
+
+                    Toast.makeText(CadastroSubEventoActivity.this, "Foto do evento está" +
+                            " carregando aguarde !!!", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    //método pára cadastrar subevento
+                    SalvadadosBd();
+
+                }
+
+            }
+        });
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //metodo para ligar a camera do celular
+                tirarfoto();
+
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //metodo para fazer upload diretamente dos arquivos do seu celular
+                abrirfotosandroid();
+            }
+        });
 
     }
-
-
 
 
 
@@ -87,4 +151,159 @@ public class CadastroSubEventoActivity extends AppCompatActivity {
         return true;
 
     }
+
+    //abre a galeria de foto do meu celular através deste método
+
+    private void abrirfotosandroid() {
+
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(i.ACTION_GET_CONTENT);
+        startActivityForResult(i, 1);
+    }
+
+
+    //tirar foto diratamente do meu celular
+
+    private void tirarfoto() {
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 2);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //se o codigo de requisição for 1 eu pego diretamente da minha galeria de fotos
+
+        if (requestCode == 1 && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+
+            mImageUri = data.getData();
+            Picasso.with(this).load(mImageUri).into(fotosubevento);
+
+            fotosubevento.setVisibility(View.VISIBLE);
+
+            //se o codigo de requisição for 2 eu consigo tirar foto pelo celular
+
+        } else if (requestCode == 2 && resultCode == RESULT_OK) {
+
+
+            imagem = (Bitmap) data.getExtras().get("data");
+            fotosubevento.setImageBitmap(imagem);
+            fotosubevento.setVisibility(View.VISIBLE);
+
+
+        }
+
+    }
+
+    //pega extensao da foto
+
+    private String getFileExtension(Uri uri) {
+
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+
+        return mime.getExtensionFromMimeType(cr.getType(uri));
+
+    }
+
+
+
+    //METODO QUE SALVA OS DADOS NO FIREBASE REALTIME E NO FIREBASESTORE
+
+    private void SalvadadosBd() {
+
+        //SE A FOTO FOR PEGA DIRETO DO APARELHO
+
+        if (mImageUri != null) {
+
+            final StorageReference filereference = stref.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+
+            SubeventosUploadTask = filereference.putFile(mImageUri)
+
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+
+                        //se a foto for sucesso manda para o banco
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            //para de executar o progressbar apos 5 segundos e o progressbar zera !!!
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progresso.setProgress(0);
+                                }
+                            }, 500);
+
+
+                            //método novo para armazenar imagens
+
+                            Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                            while (!urlTask.isSuccessful());
+                            Uri downloadUrl = urlTask.getResult();
+
+
+                            //metodo que pega tudoo da classe sub evento para registrar no firebase!!!
+
+
+                            SubEvento sub = new SubEvento(responsavel.getText().toString().trim(), local
+                                    .getText().toString().trim(), minicurriculo.getText().toString().trim(), descricaosubevento
+                                    .getText().toString().trim(), periodosubevento.getText().toString().trim(),
+                                    downloadUrl.toString(), tiposubevento.getSelectedItem().toString().trim());
+
+                            String subeventoId = ref.push().getKey();
+                            ref.child(subeventoId).setValue(sub);
+
+
+                            //sucesso ao gravar os dados
+
+                            Toast.makeText(CadastroSubEventoActivity.this, "Sub Evento cadastrado" +
+                                    " com sucesso !!!", Toast.LENGTH_LONG).show();
+
+
+                        }
+                    })
+
+                    //se não mostra a exceção de erro
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            Toast.makeText(CadastroSubEventoActivity.this,
+                                    e.getMessage(), Toast.LENGTH_LONG);
+
+                        }
+                    })
+
+
+                    //adiciona o tempo na barra de progresso
+
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            //seta o tempo da barra de progresso até a foto ir para o firebase storage
+
+                            double progress = (100 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            progresso.setProgress((int) progress);
+
+                        }
+                    });
+
+            //SE A IMAGEM FOR PEGA DIRETAMENTE DA CAMERA
+
+        }  else
+
+            Toast.makeText(CadastroSubEventoActivity.this, "Nenhuma imagem" +
+                    " selecionada !!!", Toast.LENGTH_LONG).show();
+
+    }
+
 }
